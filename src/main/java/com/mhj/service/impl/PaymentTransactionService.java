01d0694a93654067780ee7282d6f5767ad019b90
mahuijian <ma.huijian@xyb2b.com>
2020-08-28 11:26:26 +0800

    feat: test

--- src/main/java/com/mhj/service/impl/PaymentTransactionService.java
+++ src/main/java/com/mhj/service/impl/PaymentTransactionService.java
@@ -1,15 +1,21 @@
 package com.mhj.service.impl;
 
 import com.github.rholder.retry.RetryException;
+import com.google.common.util.concurrent.FutureCallback;
 import com.mhj.config.ProductRetryerBuilder;
+import com.mhj.config.ThreadExecutorConfig;
 import com.mhj.dto.PaymentDTO;
 import com.mhj.mapper.PaymentTransactionDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 
 /**
  * @author mahuijian
@@ -21,15 +27,48 @@ public class PaymentTransactionService {
 
     private PaymentTransactionDao paymentTransactionDao;
 
+    private static int NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors();
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    @Autowired
+    private ExecutorService executorService;
+
     public String generateToken(PaymentDTO paymentDTO) {
-        try {
-            ProductRetryerBuilder.build().call(()->testTask(1));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (RetryException e) {
-            e.printStackTrace();
-        }
-        return "";
+//        try {
+//            ProductRetryerBuilder.build().call(()->testTask(1));
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (RetryException e) {
+//            e.printStackTrace();
+//        }
+//        return "";
+
+        transactionTemplate.execute(transactionStatus -> {
+            log.info("执行事务中");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("执行事务中");
+            return null;
+        });
+
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(100);
+
+                System.out.println(NUMBER_OF_CORES);
+                System.out.println("异步执行中");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, executorService);
+        return "OK";
+
 
 //        // 插入一条预支付记录
 //        // 支付全局id
